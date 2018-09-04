1888ac5f4420615b00d2080214fb95792258494f
Anton Tsivarev <a.tsivarev@corp.vk.com>
2018-09-04 18:06:33 +0300

    Add VKWebAppSetViewSettings

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ export default class App extends Component {
             "VKWebAppJoinGroup",
             "VKWebAppOpenApp",
             "VKWebAppOpenQR",
+            "VKWebAppSetViewSettings",
         ];
     }
 
