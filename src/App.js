15b10efa8c19db15736ad1b77b10768beb153ff3
Anton Tsivarev <a.tsivarev@corp.vk.com>
2018-10-22 10:59:15 +0300

    Add VKWebAppClose

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ export default class App extends Component {
             "VKWebAppAllowMessagesFromGroup",
             "VKWebAppJoinGroup",
             "VKWebAppOpenApp",
+            "VKWebAppClose",
             "VKWebAppOpenQR",
             "VKWebAppSetViewSettings",
         ];
